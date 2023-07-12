85363f02d4df7e7d5199e8e483261de1eae83707
Erlon Jr <erlon.dn.jr@gmail.com>
2023-07-12 13:03:27 -0300

    bug: increase pencil size

--- app/app.py
+++ app/app.py
@@ -4,20 +4,26 @@ import pygame
 from . import settings as st
 
 
-def button_factory(path_image: str, expanded=False):
+def button_factory(
+    path_image: str,
+    expanded=False,
+    button_size=st.NORMAL_BTN_SIZE,
+    icon_size=st.BTN_ICON_SIZE,
+    padding=st.BTN_ICON_PADDING,
+):
     btn = (
-        pygame.Surface(st.NORMAL_BTN_SIZE)
+        pygame.Surface(button_size)
         if not expanded
         else pygame.Surface(st.LARGE_BUTTON_SIZE)
     )
     btn_image = pygame.image.load(path_image)
-    btn_image = pygame.transform.scale(btn_image, st.BTN_ICON_SIZE)
+    btn_image = pygame.transform.scale(btn_image, icon_size)
     btn.fill(st.BTN_COLOR)
-    btn.blit(btn_image, st.BTN_ICON_PADDING if not expanded else (50, 10))
+    btn.blit(btn_image, padding if not expanded else st.BTN_LARGE_ICON_PADDING)
 
     btn_hover = btn.copy()
     btn_hover.fill(st.BTN_HOVER_COLOR, special_flags=pygame.BLEND_ADD)
-    btn_hover.blit(btn_image, st.BTN_ICON_PADDING if not expanded else (50, 10))
+    btn_hover.blit(btn_image, padding if not expanded else st.BTN_LARGE_ICON_PADDING)
 
     return btn, btn_hover
 
@@ -32,27 +38,46 @@ def main():
     bar_tools.fill(st.BAR_BG_COLOR)
 
     # CURRENT TOOL
-    current_tool = st.PENCIL_TOOL
+    current_tool = None
+    current_color = (255, 255, 255)
 
     # BAR TOOL - BUTTONS
     pencil_btn, pencil_btn_hover = button_factory(st.PENCIL_IMAGE)
     pencil_btn_position = pygame.Rect(20.0, 20.0, *pencil_btn.get_size())
 
-    eraser_btn, eraser_btn_hover = button_factory(st.ERASER_IMAGE)
-    eraser_btn_position = pygame.Rect(100.0, 20.0, *eraser_btn.get_size())
+    increase_btn, increase_btn_hover = button_factory(
+        st.INCREASE_IMAGE,
+        button_size=st.SMALL_BTN_SIZE,
+        icon_size=st.BTN_SMALL_ICON_SIZE,
+        padding=st.BTN_SMALL_ICON_PADDING,
+    )
+    increase_btn_position = pygame.Rect(100.0, 20.0, *increase_btn.get_size())
+    decrease_btn, decrease_btn_hover = button_factory(
+        st.DECREASE_IMAGE,
+        button_size=st.SMALL_BTN_SIZE,
+        icon_size=st.BTN_SMALL_ICON_SIZE,
+        padding=st.BTN_SMALL_ICON_PADDING,
+    )
+    decrease_btn_position = pygame.Rect(130.0, 50.0, *increase_btn.get_size())
 
-    fill_btn, fill_btn_hover = button_factory(st.FILL_IMAGE)
-    fill_btn_position = pygame.Rect(20.0, 100.0, *fill_btn.get_size())
+    eraser_btn, eraser_btn_hover = button_factory(st.ERASER_IMAGE)
+    # eraser_btn_position = pygame.Rect(100.0, 20.0, *eraser_btn.get_size())
+    eraser_btn_position = pygame.Rect(20.0, 100.0, *eraser_btn.get_size())
+
+    """fill_btn, fill_btn_hover = button_factory(st.FILL_IMAGE)
+    fill_btn_position = pygame.Rect(20.0, 100.0, *fill_btn.get_size())"""
 
     clear_btn, clear_btn_hover = button_factory(st.CLEAR_IMAGE)
     clear_btn_position = pygame.Rect(100.0, 100.0, *clear_btn.get_size())
 
-    palette_btn, palette_btn_hover = button_factory(st.PALETTE_IMAGE, expanded=True)
-    palette_btn_position = pygame.Rect(20.0, 180.0, *palette_btn.get_size())
+    # COLOR INFO
+    color_info = pygame.Surface(st.LARGE_BUTTON_SIZE)
+    color_info.fill(current_color)
+    color_info_position = pygame.Rect(20.0, 180.0, *color_info.get_size())
 
     # drawing board - canvas
     canvas = pygame.Surface(st.CANVAS_SIZE)
-    canvas.fill(st.BG_COLOR)
+    canvas.fill(st.CANVAS_BG_COLOR)
 
     # window conf
     window_size = (st.WIDTH, st.HEIGHT)
@@ -70,7 +95,7 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x_pos = event.pos[0]
                 y_pos = event.pos[1]
-                
+
                 if event.button == 1 and current_tool == st.PENCIL_TOOL:
                     pygame.draw.rect(
                         canvas,
@@ -86,7 +111,7 @@ def main():
                 elif event.button == 1 and current_tool == st.ERASER_TOOL:
                     pygame.draw.rect(
                         canvas,
-                        st.BG_COLOR,
+                        st.CANVAS_BG_COLOR,
                         [
                             x_pos - (x_pos % pixel_size) - 200,
                             y_pos - (x_pos % pixel_size) - 20,
@@ -96,61 +121,81 @@ def main():
                     )
 
                 elif event.button == 1 and current_tool == st.PALETTE_TOOL:
-                    pixel_color = "red"
-                    current_tool = st.PENCIL_TOOL
+                    pass
 
-                elif event.button == 4:
+                elif event.button == 1 and increase_btn_position.collidepoint(
+                    event.pos
+                ):
                     pixel_size += 1
+                    print(pixel_size)
 
-                elif event.button == 5:
+                elif event.button == 1 and decrease_btn_position.collidepoint(
+                    event.pos
+                ):
                     if pixel_size > 1:
                         pixel_size -= 1
-                
+
                 if event.button == 1 and pencil_btn_position.collidepoint(event.pos):
                     current_tool = st.PENCIL_TOOL
+
                 elif event.button == 1 and eraser_btn_position.collidepoint(event.pos):
                     current_tool = st.ERASER_TOOL
-                elif event.button == 1 and fill_btn_position.collidepoint(event.pos):
-                    current_tool = st.FILL_TOOL
+                # elif event.button == 1 and fill_btn_position.collidepoint(event.pos):
+                #    current_tool = st.FILL_TOOL
                 elif event.button == 1 and clear_btn_position.collidepoint(event.pos):
-                    current_tool = st.CLEAR_TOOL
-                elif event.button == 1 and palette_btn_position.collidepoint(event.pos):
+                    canvas.fill(st.CANVAS_BG_COLOR)
+                elif event.button == 1 and color_info_position.collidepoint(event.pos):
                     current_tool = st.PALETTE_TOOL
 
-            
             if event.type == pygame.MOUSEMOTION:
                 if pencil_btn_position.collidepoint(event.pos):
                     pencil_btn = pencil_btn_hover
                 else:
                     pencil_btn, _ = button_factory(st.PENCIL_IMAGE)
 
+                if increase_btn_position.collidepoint(event.pos):
+                    increase_btn = increase_btn_hover
+                else:
+                    increase_btn, _ = button_factory(
+                        st.INCREASE_IMAGE,
+                        button_size=st.SMALL_BTN_SIZE,
+                        icon_size=st.BTN_SMALL_ICON_SIZE,
+                        padding=st.BTN_SMALL_ICON_PADDING,
+                    )
+
+                if decrease_btn_position.collidepoint(event.pos):
+                    decrease_btn = decrease_btn_hover
+                else:
+                    decrease_btn, _ = button_factory(
+                        st.DECREASE_IMAGE,
+                        button_size=st.SMALL_BTN_SIZE,
+                        icon_size=st.BTN_SMALL_ICON_SIZE,
+                        padding=st.BTN_SMALL_ICON_PADDING,
+                    )
+
                 if eraser_btn_position.collidepoint(event.pos):
                     eraser_btn = eraser_btn_hover
                 else:
                     eraser_btn, _ = button_factory(st.ERASER_IMAGE)
 
-                if fill_btn_position.collidepoint(event.pos):
+                """if fill_btn_position.collidepoint(event.pos):
                     fill_btn = fill_btn_hover
                 else:
-                    fill_btn, _ = button_factory(st.FILL_IMAGE)
+                    fill_btn, _ = button_factory(st.FILL_IMAGE)"""
 
                 if clear_btn_position.collidepoint(event.pos):
                     clear_btn = clear_btn_hover
                 else:
                     clear_btn, _ = button_factory(st.CLEAR_IMAGE)
 
-                if palette_btn_position.collidepoint(event.pos):
-                    palette_btn = palette_btn_hover
-                else:
-                    palette_btn, _ = button_factory(st.PALETTE_IMAGE, expanded=True)
-
-
         window.blit(bar_tools, (0, 0))
         bar_tools.blit(pencil_btn, pencil_btn_position)
+        bar_tools.blit(increase_btn, increase_btn_position)
+        bar_tools.blit(decrease_btn, decrease_btn_position)
         bar_tools.blit(eraser_btn, eraser_btn_position)
-        bar_tools.blit(fill_btn, fill_btn_position)
+        # bar_tools.blit(fill_btn, fill_btn_position)
         bar_tools.blit(clear_btn, clear_btn_position)
-        bar_tools.blit(palette_btn, palette_btn_position)
+        bar_tools.blit(color_info, color_info_position)
 
         window.blit(canvas, (200, 20))
 
--- app/settings.py
+++ app/settings.py
@@ -7,21 +7,37 @@ BAR_TOOL_SIZE = 180, HEIGHT
 CANVAS_SIZE = 1060, 640
 
 # COLORS
-BG_COLOR = (26, 26, 26)
+CANVAS_BG_COLOR = (26, 26, 26)
 BAR_BG_COLOR = (51, 51, 51)
 BTN_COLOR = (77, 77, 77)
 BTN_HOVER_COLOR = (88, 88, 88)
+
+ALL_RGB_COLORS = [(r, g, b) for r in range(256) for g in range(256) for b in range(256)]
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+
 
 # TOOLS
 PIXEL_SIZE = 10
 PIXEL_COLOR = "white"
 
 # BUTTON SIZE
+SMALL_BTN_SIZE = (30, 30)
 NORMAL_BTN_SIZE = (60, 60)
 LARGE_BUTTON_SIZE = (140, 60)
 
+BTN_SMALL_ICON_SIZE = (20, 20)
 BTN_ICON_SIZE = (40, 40)
 BTN_ICON_PADDING = (10, 10)
+BTN_SMALL_ICON_PADDING = (5, 5)
+BTN_LARGE_ICON_PADDING = (50, 10)
 
 
 # IMAGES
@@ -30,6 +46,8 @@ ERASER_IMAGE = "./app/assets/eraser-solid.svg"
 PALETTE_IMAGE = "./app/assets/palette-solid.svg"
 FILL_IMAGE = "./app/assets/fill-drip-solid.svg"
 CLEAR_IMAGE = "./app/assets/clear-solid.svg"
+INCREASE_IMAGE = "./app/assets/plus-solid.svg"
+DECREASE_IMAGE = "./app/assets/minus-solid.svg"
 
 LOGO = "./app/assets/logo.png"
 
